35923ee520291f9f07e22b809d4b98793f179c80
Achim Christ <achim.christ@de.ibm.com>
2015-03-23 08:59:51 +0100

    Added 'strict' declaration

--- genfiles.pl
+++ genfiles.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+use strict;
+
 die("usage: $0 <directory> <number_of_files> [<name_suffix>]\n")
   unless (@ARGV == 2) || (@ARGV == 3);
 
@@ -7,23 +9,25 @@ my($dir, $num_files, $suffix) = @ARGV;
 $suffix or $suffix = ".tmp";
 
 #set min size in KB
-$min_size = 100;
+my $min_size = 100;
 # set max offset size in KB
-$off_size = 1000;
+my $off_size = 1000;
 
 print "Generating $num_files files, at random size in $dir with suffix $suffix.\n";
 
-for($i = 0; $i < $num_files; $i++) {
+# TODO: Check to see if directory exists
+
+for(my $i = 0; $i < $num_files; $i++) {
 	# Compute buffer size
-	$size = $min_size * 1024;
+	my $size = $min_size * 1024;
 	$size += int(rand($off_size)) * 1024;
 
 	# Compute filename
 	my $name = "file_$i$suffix";
 
 	# Generate buffer
-	$outBuf = "";
-	for($j = 0; $j < $size; $j++) {
+	my $outBuf = "";
+	for(my $j = 0; $j < $size; $j++) {
 		# Generate random character
 		$outBuf .= chr(int(rand(256)));
 	}
