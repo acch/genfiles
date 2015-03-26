6755101474cd76e90b70c73cedf2de4d8e3da4f3
Achim Christ <achim.christ@de.ibm.com>
2015-03-26 18:46:12 +0100

    Added initial Config::General support

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Genfiles
 
 Simple tool for generating files
+
+Requires: Perl Config::General Module (perl-Config-General)
--- genfiles.pl
+++ genfiles.pl
@@ -1,45 +1,59 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
 
-die("usage: $0 <directory> <number_of_files> [<name_suffix>]\n")
+die("Usage: $0 <directory> <number_of_files> [<name_suffix>]\n")
   unless (@ARGV == 2) || (@ARGV == 3);
+
+die("Config file not found: genfiles.conf\n")
+  unless (-f "genfiles.conf");
+
+use Config::General;
+my $conf = Config::General->new(
+  -ConfigFile => "genfiles.conf",
+  -LowerCaseNames => 1);
+my %config = $conf->getall;
 
 my($dir, $num_files, $suffix) = @ARGV;
 $suffix or $suffix = ".tmp";
 
-#set min size in KB
+# set min size in KB
 my $min_size = 100;
 # set max offset size in KB
 my $off_size = 1000;
 
-print "Generating $num_files files, at random size in $dir with suffix $suffix.\n";
+print "Generating $num_files files, at random size in $dir with suffix $suffix...\n";
 
-# TODO: Check to see if directory exists
+# check to see if directory exists
+unless (-d $dir) {
+  mkdir $dir
+    or die("Can't create $dir: $!");
+}
 
 for(my $i = 0; $i < $num_files; $i++) {
-	# Compute buffer size
+	# compute buffer size
 	my $size = $min_size * 1024;
 	$size += int(rand($off_size)) * 1024;
 
-	# Compute filename
+	# compute filename
 	my $name = "file_$i$suffix";
 
-	# Generate buffer
+	# generate buffer
 	my $outBuf = "";
 	for(my $j = 0; $j < $size; $j++) {
-		# Generate random character
+		# generate random character
 		$outBuf .= chr(int(rand(256)));
 	}
 
-	# Open file
+	# open file
 	open(FH, ">", $dir.$name)
 		or die("Can't open $dir.$name: $!");
 
-	# Write buffer to file
+	# write buffer to file
 	print(FH $outBuf);
 
-	# Close file
+	# close file
 	close(FH);
 }
 
